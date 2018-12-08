6f193e40f7031314d48a893ddabccddbb370f67d
Jeff Winn <jeff.winn@gmail.com>
2018-12-08 16:27:09 -0500

    Updating the intellisense comments.

--- src/DotRas/DisposableObject.cs
+++ src/DotRas/DisposableObject.cs
@@ -2,6 +2,9 @@
 
 namespace DotRas
 {
+    /// <summary>
+    /// Provides a base class for an object which is disposable. This class must be inherited.
+    /// </summary>
     public abstract class DisposableObject : IDisposable
     {
         private bool disposed;
@@ -11,12 +14,17 @@ namespace DotRas
             Dispose(false);
         }
 
+        /// <inheritdoc />
         public void Dispose()
         {
             Dispose(true);
             GC.SuppressFinalize(this);
         }
 
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or releasing unmanaged resources.
+        /// </summary>
+        /// <param name="disposing">true to release both managed and unmanaged resources, false to release only unmanaged resources.</param>
         protected virtual void Dispose(bool disposing)
         {
             if (disposing)
