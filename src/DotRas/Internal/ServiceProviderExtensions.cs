d5445fd17cd71fe50d4c11d938e9e493e25ff52f
Jeff Winn <jeff.winn@gmail.com>
2019-01-14 08:26:08 -0500

    Moving the service provider extensions.

---
+++ src/DotRas/Internal/ServiceProviderExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DotRas.Internal
+{
+    internal static class ServiceProviderExtensions
+    {
+        public static T GetService<T>(this IServiceProvider serviceProvider)
+        {
+            return (T)serviceProvider.GetService(typeof(T));
+        }
+
+        public static object GetRequiredService(this IServiceProvider serviceProvider, Type serviceType)
+        {
+            if (serviceType == null)
+            {
+                throw new ArgumentNullException(nameof(serviceType));
+            }
+
+            var result = serviceProvider.GetService(serviceType);
+            if (result == null)
+            {
+                throw new InvalidOperationException($"The required service '{serviceType}' could not be found.");
+            }
+
+            return result;
+        }
+
+        public static T GetRequiredService<T>(this IServiceProvider serviceProvider)
+        {
+            return (T)serviceProvider.GetRequiredService(typeof(T));
+        }
+    }
+}
---
+++ test/DotRas.Tests/Internal/ServiceProviderExtensionsTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using DotRas.Internal.Composition;
+using DotRas.Internal.Interop;
+using DotRas.Tests.Internal.Stubs;
+using Moq;
+using NUnit.Framework;
+
+namespace DotRas.Tests.Internal
+{
+    [TestFixture]
+    public class ServiceProviderExtensionsTests
+    {
+        [Test]
+        public void RetrievesTheObjectForTheGenericTypeSpecified()
+        {
+            var result = new Mock<IRasApi32>();
+
+            var target = new StubServiceProvider(result.Object);
+            var actual = target.GetService<IRasApi32>();
+
+            Assert.AreEqual(result.Object, actual);
+        }
+
+        [Test]
+        public void ReturnNullWhenExpected()
+        {
+            var target = new StubServiceProvider(null);
+            var result = target.GetService<IRasApi32>();
+
+            Assert.IsNull(result);
+        }
+
+        [Test]
+        public void ThrowsAnExceptionWhenServiceTypeIsNull()
+        {
+            var serviceProvider = new StubServiceProvider(null);
+            Assert.Throws<ArgumentNullException>(() => serviceProvider.GetRequiredService(null));
+        }
+
+        [Test]
+        public void ThrowsAnExceptionWhenTheServiceIsNotFoundWhenRequired()
+        {
+            var serviceProvider = new StubServiceProvider(null);
+            Assert.Throws<InvalidOperationException>(() => serviceProvider.GetRequiredService<IRasApi32>());
+        }
+    }
+}
