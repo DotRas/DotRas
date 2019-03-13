20f55536e52a033b0172982693f23fc3343f4df0
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2019-03-13 08:53:19 -0400

    Resolves #32.

---
+++ examples/DialConnectionAndWatchForDisconnect/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading;
+using DotRas;
+
+namespace DialConnectionAndWatchForDisconnect
+{
+    class Program : IDisposable
+    {
+        private readonly RasDialer dialer;
+        private readonly RasConnectionWatcher watcher;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (var p = new Program())
+                {
+                    p.Run();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            Console.WriteLine("Press any key to terminate...");
+            Console.ReadKey(true);
+        }
+
+        public Program()
+        {
+            dialer = new RasDialer();
+
+            watcher = new RasConnectionWatcher();
+            watcher.Disconnected += OnConnectionDisconnected;
+        }
+
+        ~Program()
+        {
+            Dispose(false);
+        }
+
+        private void Run()
+        {
+            // This should contain the name.
+            dialer.EntryName = "Your Entry";
+
+            // If your account requires credentials that have not been persisted, they can be passed here.
+            dialer.Credentials = new NetworkCredential("Username", "Password");
+
+            // This specifies the default location for Windows phone books.
+            dialer.PhoneBookPath = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                @"Microsoft\Network\Connections\Pbk\rasphone.pbk");
+            
+            // Dials the connection synchronously. This will still raise events, and it will also allow for timeouts
+            // if the cancellation token is passed into the api via the overload.
+            var connection = dialer.Connect();
+
+            Console.WriteLine($"Connected: [{connection.EntryName}] @ {connection.Handle}");
+
+            watcher.Connection = connection;
+            watcher.Start();
+
+            // This will just force disconnect, however this could also be external if the connection is dropped due to the
+            // network on the machine being physically disconnected.
+            Console.WriteLine("Just waiting for a bit before forcing disconnect...");
+            Thread.Sleep(TimeSpan.FromSeconds(10));
+
+            connection.Disconnect();
+            watcher.Stop();
+        }
+
+        private void OnConnectionDisconnected(object sender, RasConnectionEventArgs e)
+        {
+            Console.WriteLine($"Disconnected: [{e.ConnectionInformation.EntryName}] @ {e.ConnectionInformation.Handle}");
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                dialer.Dispose();
+                watcher.Dispose();
+            }
+        }
+    }
+}
