16b6dde51d211588547ea4481c4a01239f34bf80
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2019-05-28 13:32:04 -0400

    Fixing build server compilation issue.

--- src/DotRas/Internal/Infrastructure/Primitives/ValueWaiter.cs
+++ src/DotRas/Internal/Infrastructure/Primitives/ValueWaiter.cs
@@ -21,7 +21,7 @@ namespace DotRas.Internal.Primitives
         {
             lock (syncRoot)
             {
-                Value = default;
+                Value = default(T);
                 IsSet = false;
             }
 
