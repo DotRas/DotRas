e51f24f76784a0084890439d85c8eedf50a3f074
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2020-03-01 07:59:33 -0500

    #40 - Adding additional unit tests.

--- test/DotRas.Tests/RasDeviceTests.cs
+++ test/DotRas.Tests/RasDeviceTests.cs
@@ -1,4 +1,8 @@
-﻿using DotRas.Tests.Stubs;
+﻿using System;
+using DotRas.Internal;
+using DotRas.Internal.Abstractions.Services;
+using DotRas.Tests.Stubs;
+using Moq;
 using NUnit.Framework;
 
 namespace DotRas.Tests
@@ -6,6 +10,24 @@ namespace DotRas.Tests
     [TestFixture]
     public class RasDeviceTests
     {
+        private Mock<IServiceProvider> container;
+        private Mock<IRasEnumDevices> rasEnumDevices;
+
+        [SetUp]
+        public void Setup()
+        {
+            rasEnumDevices = new Mock<IRasEnumDevices>();
+
+            container = new Mock<IServiceProvider>();
+            ServiceLocator.Default = container.Object;
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            ServiceLocator.Clear();
+        }
+
         [Test]
         public void ShouldNotThrowAnExceptionWhenTheDeviceNameIsNull()
         {
@@ -25,6 +47,19 @@ namespace DotRas.Tests
         {
             var target = new TestDevice(string.Empty);
             Assert.AreEqual(string.Empty, target.Name);
+        }
+
+        [Test]
+        public void EnumeratesTheDevicesCorrectly()
+        {
+            rasEnumDevices.Setup(o => o.EnumerateDevices()).Returns(new RasDevice[0]);
+
+            container.Setup(o => o.GetService(typeof(IRasEnumDevices))).Returns(rasEnumDevices.Object);
+            var result = RasDevice.EnumerateDevices();
+
+            Assert.IsNotNull(result);
+            container.Verify(o => o.GetService(typeof(IRasEnumDevices)));
+            rasEnumDevices.Verify(o => o.EnumerateDevices());
         }
     }
 }
