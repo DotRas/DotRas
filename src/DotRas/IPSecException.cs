c10d7c3093235ec61e162242851c9cb407d75a62
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2020-07-21 07:15:33 -0400

    Adding IPSecException for IPSec error code ranges.

---
+++ src/DotRas/IPSecException.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.Serialization;
+
+namespace DotRas
+{
+    /// <summary>
+    /// Thrown when an IPSec related error code occurs within the operating system.
+    /// </summary>
+    [Serializable]
+    public class IPSecException : Win32Exception
+    {
+        /// <summary>
+        /// Initializes an instance of the <see cref="IPSecException"/> class.
+        /// </summary>
+        public IPSecException()
+        {
+        }
+
+        /// <summary>
+        /// Initializes an instance of the <see cref="IPSecException"/> class.
+        /// </summary>
+        /// <param name="message">A message describing the error.</param>
+        public IPSecException(string message)
+            : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Initializes an instance of the <see cref="IPSecException"/> class.
+        /// </summary>
+        /// <param name="error">The Win32 error code.</param>
+        public IPSecException(int error)
+            : base(error)
+        {
+        }
+
+        /// <summary>
+        /// Initializes an instance of the <see cref="IPSecException"/> class.
+        /// </summary>
+        /// <param name="error">The Win32 error code.</param>
+        /// <param name="message">A message describing the error.</param>
+        public IPSecException(int error, string message)
+            : base(error, message)
+        {
+        }
+
+        /// <summary>
+        /// Initializes an instance of the <see cref="IPSecException"/> class.
+        /// </summary>
+        /// <param name="message">A message describing the error.</param>
+        /// <param name="innerException">An exception which is the cause of this exception.</param>
+        public IPSecException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        /// <inheritdoc />
+        protected IPSecException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
--- src/DotRas/Internal/Interop/WinError.cs
+++ src/DotRas/Internal/Interop/WinError.cs
@@ -11,5 +11,15 @@
         public const int ERROR_INSUFFICIENT_BUFFER = 122;
 
         public const int ERROR_ALREADY_EXISTS = 183;
+
+        /// <summary>
+        /// CUSTOM: Identifies the start range of the IPSec errors.
+        /// </summary>
+        public const int IPSECBASE = 13000;
+
+        /// <summary>
+        /// CUSTOM: Identifies the end range of the IPSec errors.
+        /// </summary>
+        public const int IPSECBASEEND = 13999;
     }
 }
