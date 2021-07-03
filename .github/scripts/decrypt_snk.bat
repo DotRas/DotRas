a243df997e93f0b3a131f7b72843a06618eea9b0
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2021-07-03 11:37:59 -0400

    Updating cipher algorithm.

--- .github/scripts/decrypt_snk.bat
+++ .github/scripts/decrypt_snk.bat
@@ -1,2 +1,2 @@
 gpg --version
-gpg --quiet --batch --yes --decrypt --passphrase="$SNK_PASSPHRASE" --output Winnster.snk Winnster.snk.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$SNK_PASSPHRASE" --symmetric --cipher-algo AES256 --output Winnster.snk Winnster.snk.gpg
