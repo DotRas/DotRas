8f88bc0165cc290028f9f31647cd4b3fa0b17bff
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2021-07-03 11:25:24 -0400

    Fixing script.

--- .github/scripts/decrypt_snk.bat
+++ .github/scripts/decrypt_snk.bat
@@ -1,3 +1,4 @@
-# --batch to prevent interactive command
-# --yes to assume "yes" for questions
+REM --batch to prevent interactive command
+REM --yes to assume "yes" for questions
+
 gpg --quiet --batch --yes --decrypt --passphrase="$SNK_PASSPHRASE" --output Winnster.snk Winnster.snk.gpg
