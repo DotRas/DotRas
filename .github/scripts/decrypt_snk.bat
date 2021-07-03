71c159014fb248a575ebeeeef9f1ce3990ea8375
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2021-07-03 11:22:31 -0400

    Adding decrypt step.

---
+++ .github/scripts/decrypt_snk.bat
@@ -0,0 +1,3 @@
+# --batch to prevent interactive command
+# --yes to assume "yes" for questions
+gpg --quiet --batch --yes --decrypt --passphrase="$SNK_PASSPHRASE" --output ../../Winnster.snk ../../Winnster.snk.gpg
