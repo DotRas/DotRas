97471ddd75d0858ca5e89c2e406450fe3388de7f
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2021-07-03 11:26:44 -0400

    Removing script comments.

--- .github/scripts/decrypt_snk.bat
+++ .github/scripts/decrypt_snk.bat
@@ -1,4 +1,1 @@
-REM --batch to prevent interactive command
-REM --yes to assume "yes" for questions
-
 gpg --quiet --batch --yes --decrypt --passphrase="$SNK_PASSPHRASE" --output Winnster.snk Winnster.snk.gpg
