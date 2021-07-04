8533bf16688f9f3ebcaf29e5e621e03b8f63b4ca
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2021-07-03 21:11:08 -0400

    Updating pipeline.

--- .github/scripts/decrypt_snk.bat
+++ .github/scripts/decrypt_snk.bat
@@ -1,2 +1,2 @@
 gpg --version
-gpg --quiet --batch --yes --passphrase %SNK_PASSPHRASE% --output Winnster.snk -d Winnster.snk.gpg
+gpg --quiet --batch --yes --passphrase %SNK_PASSPHRASE% --output Winnster.snk -d ./.github/secrets/Winnster.snk.gpg
