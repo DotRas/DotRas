a5e7a494ce6e6e7da3b48f770aad5671c10aa53d
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2024-04-05 17:14:18 -0400

    Adding logging and configuration support.

--- src/ConsoleRunner/Program.IoC.cs
+++ src/ConsoleRunner/Program.IoC.cs
@@ -1,23 +1,30 @@
 ﻿using System;
 using ConsoleRunner.Infrastructure.Diagnostics;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Configuration;
 
 namespace ConsoleRunner
 {
     partial class Program
     {
         private static IServiceProvider applicationServices;
+        private static IConfiguration configuration;
 
         private static void ConfigureIoC()
         {
+            configuration = new ConfigurationBuilder()
+                .AddJsonFile("appSettings.json")
+                .Build();
+
             var services = new ServiceCollection();
             services.AddTransient<DotRasLoggingAdapter>();
+            services.AddSingleton(configuration);
 
-            services.AddLogging(builder =>
-            {
-                builder.SetMinimumLevel(LogLevel.Debug);
-            });
+            services.AddLogging(builder => builder
+                .AddConfiguration(configuration.GetSection("Logging"))
+                .AddSimpleConsole());
 
             applicationServices = services.BuildServiceProvider();
         }
