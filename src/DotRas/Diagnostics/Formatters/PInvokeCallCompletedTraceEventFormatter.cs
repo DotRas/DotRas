4e7a0a9c7fae5fbd9225a47a760f4fa35b44a1a6
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2024-06-30 21:55:19 -0400

    Fixing messages.

--- src/DotRas/Diagnostics/Formatters/PInvokeCallCompletedTraceEventFormatter.cs
+++ src/DotRas/Diagnostics/Formatters/PInvokeCallCompletedTraceEventFormatter.cs
@@ -23,22 +23,22 @@ namespace DotRas.Diagnostics.Formatters
             var sb = new StringBuilder();
 
             sb.Append("PInvoke external call completed. See the following for more details:");
-            sb.AppendLine().Append($"\tDllName: {eventData.DllName}");
-            sb.AppendLine().Append($"\tEntryPoint: '{eventData.MethodName}'");
-            sb.AppendLine().Append($"\tReturnCode: [{eventData.Result}]");
-            sb.AppendLine().Append($"\tDuration: {eventData.Duration}");
+            sb.Append($"\n\tDllName: {eventData.DllName}");
+            sb.Append($"\n\tEntryPoint: '{eventData.MethodName}'");
+            sb.Append($"\n\tReturnCode: [{eventData.Result}]");
+            sb.Append($"\n\tDuration: {eventData.Duration}");
 
             if (eventData.Args.Count > 0 || eventData.OutArgs.Count > 0)
             {
-                sb.AppendLine().Append("\tArguments:");
+                sb.Append("\n\tArguments:");
                 foreach (var arg in eventData.Args)
                 {
-                    sb.AppendLine().Append($"\t\t{arg.Key}: [{arg.Value ?? "(null)"}]");
+                    sb.Append($"\n\t\t{arg.Key}: [{arg.Value ?? "(null)"}]");
                 }
 
                 foreach (var arg in eventData.OutArgs)
                 {
-                    sb.AppendLine().Append($"\t\tout {arg.Key}: [{arg.Value ?? "(null)"}]");
+                    sb.Append($"\n\t\tout {arg.Key}: [{arg.Value ?? "(null)"}]");
                 }
             }
 
--- src/DotRas/Diagnostics/Formatters/StructMarshalledToPtrTraceEventFormatter.cs
+++ src/DotRas/Diagnostics/Formatters/StructMarshalledToPtrTraceEventFormatter.cs
@@ -20,16 +20,16 @@ namespace DotRas.Diagnostics.Formatters
             var sb = new StringBuilder();
 
             sb.Append("Structure marshalled to pointer. See the following for more details:");
-            sb.AppendLine().Append($"\tType: {eventData.StructureType.FullName}");
-            sb.AppendLine().Append($"\tResult: {eventData.Result}");
-            sb.AppendLine().Append($"\tDuration: {eventData.Duration}");
+            sb.Append($"\n\tType: {eventData.StructureType.FullName}");
+            sb.Append($"\n\tResult: {eventData.Result}");
+            sb.Append($"\n\tDuration: {eventData.Duration}");
 
             if (eventData.Fields.Count > 0)
             {
-                sb.AppendLine().Append("\tFields:");
+                sb.Append("\n\tFields:");
                 foreach (var field in eventData.Fields)
                 {
-                    sb.AppendLine().Append($"\t\t{field.Key}: [{field.Value ?? "(null)"}]");
+                    sb.Append($"\n\t\t{field.Key}: [{field.Value ?? "(null)"}]");
                 }
             }
 
